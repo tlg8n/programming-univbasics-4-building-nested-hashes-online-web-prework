70300cc6f8a78f797e273a8c4741ab31f9fa874d
John Schmidley <tlg8n@outlook.com>
2019-09-08 22:19:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_third_tier
     {
       :reading_railroad=>
       {
-        "mortgage_value"=>"$100"
+        #"mortgage_value"=>"$100"
       },
       :pennsylvania_railroad=>
       {
