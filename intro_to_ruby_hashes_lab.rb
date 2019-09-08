5b27c8f3acfcfaaa0dca15c152e07102a0c9e9ef
John Schmidley <tlg8n@outlook.com>
2019-09-08 22:20:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,15 +43,15 @@ def monopoly_with_third_tier
       },
       :pennsylvania_railroad=>
       {
-        "mortgage_value"=>"$200"
+        #"mortgage_value"=>"$200"
       },
       :b_and_o_railroad=>
       {
-        "mortgage_value"=>"$400"
+        #"mortgage_value"=>"$400"
       },
       :shortline=>
       {
-        "mortgage_value"=>"$800"
+        #"mortgage_value"=>"$800"
       }
     }
   }
